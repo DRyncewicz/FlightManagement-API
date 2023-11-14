294d435307f43071188ace73b78a1d0978b02a63
Dominik Ryryry <ereence@gmail.com>
2023-11-14 20:57:13 +0100

    version migration: update all project to .net 8.0.0 version

---
+++ FlightManagement-API.Persistence/FlightDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightManagement_API.Persistence
+{
+    public class FlightDbContext : DbContext
+    {
+
+        public FlightDbContext(DbContextOptions<FlightDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ FlightManagement-API/Controllers/FlightController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightManagement_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyAllowSpecificOrigins")]
+    public class FlightController : ControllerBase
+    {
+
+    }
+}
